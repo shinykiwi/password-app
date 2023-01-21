e7abecd4da5d156279252c5bb2523253007220d3
lucasadilla <58784755+lucasadilla@users.noreply.github.com>
2023-01-21 15:17:07 -0500

    Create password_time.js

---
+++ password_time.js
@@ -0,0 +1,23 @@
+function calculateTimeToCrack(password, guessesPerSecond) {
+    // Get the number of possible characters for the password
+    let  characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%&*?()[]{}";
+    let characterCount = characters.length;
+
+    // Get the number of possible combinations for the password
+    let combinationCount = Math.pow(characterCount, password.length);
+
+    // Calculate the time to crack the password in seconds
+    let timeToCrack = combinationCount / guessesPerSecond;
+
+    // Convert time to crack to minutes
+    let timeToCrackInMinutes = timeToCrack / 60;
+
+    // Return the time to crack in minutes
+    return timeToCrackInMinutes;
+}
+
+// Example usage:
+let password = "11111111";
+let guessesPerSecond = 100000000; // 100 million guesses per second
+let timeToCrack = calculateTimeToCrack(password, guessesPerSecond);
+console.log("Time to crack password: " + timeToCrack + " minutes");
